586125092ab986c8be95cb557022083b5606c1b7
Maria Júlia Soares De Grandi <82612544+maju-degrandi@users.noreply.github.com>
2024-07-02 11:39:08 -0300

    bcmd exercise

--- bcmd/utils.c
+++ bcmd/utils.c
@@ -1,12 +1,13 @@
 /*
  *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
- *   
+ *    SPDX-FileCopyrightText: 2024 Maria Júlia De Grandi <maju.degrandi@usp.br>
+ *
  *    SPDX-License-Identifier: GPL-3.0-or-later
  *
- *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Maria Júlia De Grandi <maju.degrandi@usp.br>
  */
-
-
 
 int __attribute__((fastcall)) strcmp(const char *s1, const char *s2, int n)
 {
